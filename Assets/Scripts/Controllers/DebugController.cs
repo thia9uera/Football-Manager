e0f22370b1f5bcf581c6bd8f5f16f3a1341c88b6
thiago <thiago@graphiconic.com>
2018-08-09 23:28:02 +0100

    Debug maroto

---
+++ Assets/Scripts/Controllers/DebugController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugController : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI textField;
+
+    public void Toggle()
+    {
+        textField.text = MainController.Instance.Match.DebugString;
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Controllers/MatchController.cs
+++ Assets/Scripts/Controllers/MatchController.cs
@@ -94,6 +94,8 @@ public class MatchController : MonoBehaviour
     private bool isScorerAnnounced = false;
     private bool isHalfTime = false;
 
+    public string DebugString;
+
     public void Populate(TeamData _homeTeam, TeamData _awayTeam)
     {
         HomeTeam = _homeTeam;
@@ -125,12 +127,29 @@ public class MatchController : MonoBehaviour
         Score.UpdateScore(HomeTeam.Name, homeTeamScore, ColorUtility.ToHtmlStringRGB(HomeTeam.PrimaryColor), AwayTeam.Name, awayTeamScore, ColorUtility.ToHtmlStringRGB(AwayTeam.PrimaryColor));
     }
 
+    private bool hasStarted = false;
+    public void HandleStartButton()
+    {
+        hasStarted = !hasStarted;
+
+        if(hasStarted)
+        {
+            KickOff();
+        }
+        else
+        {
+            CancelInvoke();
+        }
+    }
+
     public void KickOff()
     {
         Reset();
-        startBtn.SetActive(false);
+        //startBtn.SetActive(false);
         Narration.UpdateNarration("KICK OFF!", Color.gray);
         InvokeRepeating("DefineActions", 1f, 1f);
+
+        DebugString = "KICK OFF! \n \n";
     }
 
     private void DefineActions()
@@ -157,6 +176,7 @@ public class MatchController : MonoBehaviour
             MarkingType marking = GetMarkingType();
             if (marking == MarkingType.Steal)
             {
+                DebugString += "\n ________________________________\n ROUBADA DE BOLA! \n ________________________________\n \n";
                 Narration.UpdateNarration(defendingPlayer.FirstName + " ROUBA A BOLA DE " + attackingPlayer.FirstName, defendingTeam.PrimaryColor);
 
                 SwitchPossesion();
@@ -164,21 +184,27 @@ public class MatchController : MonoBehaviour
             }
             else
             {
+                if(marking == MarkingType.Close) DebugString += "\n MARCACAO DE PERTO. \n \n";
+                else DebugString += "\n MARCACAO A DISTACIA. \n \n";
                 offensiveAction = GetOffensiveAction(marking);
                 if (IsActionSuccessful(marking))
                 {
                     switch (offensiveAction)
                     {
                         case PlayerData.PlayerAction.Pass:
+                            DebugString += "PASSOU A BOLA! \n ________________________________\n";
                             Narration.UpdateNarration(attackingPlayer.FirstName + " PASSA A BOLA", attackingTeam.PrimaryColor);
                             break;
                         case PlayerData.PlayerAction.Dribble:
+                            DebugString += "DRIBLOU! \n ________________________________\n";
                             Narration.UpdateNarration(attackingPlayer.FirstName + " DRIBLA " + defendingPlayer.FirstName, attackingTeam.PrimaryColor);
                             break;
                         case PlayerData.PlayerAction.Cross:
+                            DebugString += "CRUZOU! \n ________________________________\n";
                             Narration.UpdateNarration(attackingPlayer.FirstName + " CRUZA A BOLA", attackingTeam.PrimaryColor);
                             break;
                         case PlayerData.PlayerAction.Shot:
+                            DebugString += "CHUTOU! \n ________________________________\n";
                             Narration.UpdateNarration(attackingPlayer.FirstName + " TENTA O CHUTE...", attackingTeam.PrimaryColor);
                             break;
                     }
@@ -189,15 +215,19 @@ public class MatchController : MonoBehaviour
                     switch (offensiveAction)
                     {
                         case PlayerData.PlayerAction.Pass:
+                            DebugString += "PASSE BLOQUEADO! \n ________________________________\n";
                             Narration.UpdateNarration(defendingPlayer.FirstName + " BLOQUEIA O PASSE", defendingTeam.PrimaryColor);
                             break;
                         case PlayerData.PlayerAction.Dribble:
+                            DebugString += "DRIBLE DESARMADO! \n ________________________________\n";
                             Narration.UpdateNarration(defendingPlayer.FirstName + " PARA O DRIBLE DE " + attackingPlayer.FirstName, defendingTeam.PrimaryColor);
                             break;
                         case PlayerData.PlayerAction.Cross:
+                            DebugString += "CRUZAMENTO BLOQUEADO! \n ________________________________\n";
                             Narration.UpdateNarration(defendingPlayer.FirstName + " IMPEDE O CRUZAMENTO", defendingTeam.PrimaryColor);
                             break;
                         case PlayerData.PlayerAction.Shot:
+                            DebugString += "CHUTE BLOQUEADO! \n ________________________________\n";
                             Narration.UpdateNarration(defendingPlayer.FirstName + " BLOQUEIA O CHUTE DE " + attackingPlayer.FirstName, defendingTeam.PrimaryColor);
                             break;
                     }
@@ -214,7 +244,7 @@ public class MatchController : MonoBehaviour
 
     }
 
-    private int RollDice(int _sides, int _amount = 1, RollType _rollType = RollType.None, int _bonus = 0, int _bonusChance = 100)
+    private int RollDice(int _sides, int _amount = 1, RollType _rollType = RollType.None, int _bonus = 0, int _bonusChance = 101)
     {
         int n = 0;
         int roll;
@@ -299,28 +329,34 @@ public class MatchController : MonoBehaviour
         float pass = zoneChance.Pass + attackingPlayer.Prob_Pass;
         if (attackingPlayer.Passing > 70) pass += ((100 - attackingPlayer.Passing) / 100) * (attackingPlayer.Fatigue / 100);
         if (_marking == MarkingType.Close) pass = pass * 2;
-        pass = RollDice(20, 1, RollType.None, Mathf.FloorToInt(pass));
+        //pass = RollDice(20, 1, RollType.None, Mathf.FloorToInt(pass));
         if (pass > higher) action = PlayerData.PlayerAction.Pass;
 
         float dribble = zoneChance.Dribble + attackingPlayer.Prob_Dribble;
         if (attackingPlayer.Dribbling > 70) dribble += ((100 - attackingPlayer.Dribbling) / 100) * (attackingPlayer.Fatigue / 100);
         if (_marking == MarkingType.Close) dribble = dribble * 0.5f;
         else if (_marking == MarkingType.Distance) dribble = dribble * 2;
-        dribble = RollDice(20, 1, RollType.None, Mathf.FloorToInt(dribble));
+        //dribble = RollDice(20, 1, RollType.None, Mathf.FloorToInt(dribble));
         if (dribble > higher) action = PlayerData.PlayerAction.Dribble;
 
-        float crossing = zoneChance.Cross + attackingPlayer.Prob_Crossing;
-        if (attackingPlayer.Crossing > 70) crossing += ((100 - attackingPlayer.Crossing) / 100) * (attackingPlayer.Fatigue / 100);
-        if (_marking == MarkingType.Close) crossing = crossing * 0.5f;
-        crossing = RollDice(20, 1, RollType.None, Mathf.FloorToInt(crossing));
-        if (crossing > higher) action = PlayerData.PlayerAction.Cross;
+        float cross = zoneChance.Cross + attackingPlayer.Prob_Crossing;
+        if (attackingPlayer.Crossing > 70) cross += ((100 - attackingPlayer.Crossing) / 100) * (attackingPlayer.Fatigue / 100);
+        if (_marking == MarkingType.Close) cross = cross * 0.5f;
+        //cross = RollDice(20, 1, RollType.None, Mathf.FloorToInt(cross));
+        if (cross > higher) action = PlayerData.PlayerAction.Cross;
 
         float shoot = zoneChance.Shot + attackingPlayer.Prob_Shoot;
         if (attackingPlayer.Shooting > 70 && zoneChance.Shot > 0) shoot += ((100 - attackingPlayer.Shooting) / 100) * (attackingPlayer.Fatigue / 100);
         if (_marking == MarkingType.Close) shoot = shoot * 0.5f;
         else if (_marking == MarkingType.Distance) shoot = shoot * 1.5f;
-        shoot = RollDice(20, 1, RollType.None, Mathf.FloorToInt(shoot));
+        //shoot = RollDice(20, 1, RollType.None, Mathf.FloorToInt(shoot));
         if (shoot > higher) action = PlayerData.PlayerAction.Shot;
+
+        DebugString += "<size=28>" + attackingPlayer.FirstName + " " + attackingPlayer.LastName + "</size> \n \n";
+        DebugString += "Pass: " + pass + "\n";
+        DebugString += "Dribble: " + dribble + "\n";
+        DebugString += "Cross: " + cross + "\n";
+        DebugString += "Shoot: " + shoot + "\n \n";
 
         return action;
     }
